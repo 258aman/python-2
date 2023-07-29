e5dc08876db59d80451402311f5b42c4a10dd09c
siddhant <vishalsahu258@gmail.com>
2023-07-29 13:44:58 +0530

    today codes

---
+++ t1.py
@@ -0,0 +1,30 @@
+from turtle import *
+
+fd(120)
+lt(90)
+fd(120)
+lt(90)
+fd(120)
+lt(90)
+fd(120)
+lt(90)
+
+hideturtle()
+
+
+from turtle import *
+
+fd(120)
+lt(60)
+fd(120)
+lt(60)
+fd(120)
+lt(60)
+fd(120)
+lt(60)
+fd(120)
+lt(60)
+fd(120)
+lt(60)
+
+
---
+++ t2.py
@@ -0,0 +1,8 @@
+from turtle import *
+
+for i in range(6):
+    fd(120)
+    lt(60)
+
+hideturtle()
+mainloop()
